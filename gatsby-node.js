f180426ee2821c4aed51c12a63f5f194e9493e58
cleengaach <martinminarovic87@gmail.com>
2021-10-27 15:08:07 +0200

    blog start

---
+++ gatsby-node.js
@@ -0,0 +1,26 @@
+exports.createPages = async ({ actions }) => {
+  const { createPage } = actions
+  createPage({
+    path: "/using-dsg",
+    component: require.resolve("./src/templates/using-dsg.js"),
+    context: {},
+    defer: true,
+  })
+}
+exports.onCreateWebpackConfig = ({ actions, plugins, stage }) => {
+  actions.setWebpackConfig({
+    resolve: {
+      alias: {
+        path: require.resolve("path-browserify"),
+      },
+      fallback: {
+        fs: false,
+      },
+    },
+  })
+  if (stage === "build-javascript" || stage === "develop") {
+    actions.setWebpackConfig({
+      plugins: [plugins.provide({ process: "process/browser" })],
+    })
+  }
+}
