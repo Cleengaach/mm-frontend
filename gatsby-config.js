deadbc7e5c3ade9997b4c6ba20b4dfed6f6a4355
cleengaach <martinminarovic87@gmail.com>
2022-03-02 20:57:11 +0100

    api url set

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,7 +22,9 @@ module.exports = {
     {
       resolve: "gatsby-source-strapi",
       options: {
-        apiURL: process.env.API_URL || "http://localhost:1337",
+        apiURL: process.env.DEPLOY_URL
+        ? "https://YOUR-APP-URL.herokuapp.com"
+        : "http://localhost:1337",
         collectionTypes: ["article", "routes", "points"],
         singleTypes: [`homepage`, `global`],
         queryLimit: 1000,
