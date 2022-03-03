5777ed6079f22a5e697e550dae40f039d6ebf72e
cleengaach <martinminarovic87@gmail.com>
2022-03-03 20:27:38 +0100

    pridanie pluginov

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,51 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "mapProject",
   },
-  plugins: [],
+  plugins: [
+    `gatsby-plugin-react-helmet`, `gatsby-plugin-sass`,
+    `gatsby-plugin-image`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: "gatsby-source-strapi",
+      options: {
+        apiURL: process.env.API_URL,
+        collectionTypes: ["article", "routes", "points"],
+        singleTypes: [`homepage`, `global`],
+        queryLimit: 1000,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        // This will impact how browsers show your PWA/website
+        // https://css-tricks.com/meta-theme-color-and-trickery/
+        // theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: "gatsby-plugin-react-svg",
+      options: {
+        rule: {
+          include: /\.inline\.svg$/
+        }
+      }
+    }
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
 };
