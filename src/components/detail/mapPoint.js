79b34d74f6d262b71a009f3ffed51b3cfaf2629b
cleengaach <martinminarovic87@gmail.com>
2022-04-11 16:30:26 +0200

    mapa bodu

--- src/components/detail/mapPoint.js
+++ src/components/detail/mapPoint.js
@@ -66,7 +66,7 @@ const MapPoint = ({ north, east, mountain }) => {
                             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                         />
-                        <Marker position={[north, east]} icon={new Icon({iconUrl: markerIconPng, iconSize: [25, 41], iconAnchor: [12, 41]})}>
+                        <Marker position={[north, east]}>
                             <Popup>
                                 A pretty CSS3 popup. <br /> Easily customizable.
                             </Popup>
